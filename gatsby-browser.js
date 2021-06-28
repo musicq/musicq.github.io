b8ec5679c81041794d47b26387f9f27eaada0027
musicq <liang.kui@hotmail.com>
2021-06-28 14:09:29 +0800

    fix typo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 // custom typefaces
-import 'typeface-montserrat';
-import 'typeface-merriweather';
-import 'prismjs/themes/prism-solarizedlight.css';
+import 'prismjs/themes/prism.css'
+import 'typeface-merriweather'
+import 'typeface-montserrat'
+import './style.css'
+
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,5 +1,4 @@
 export default {
-
   fonts: {
     body:
       '"IBM Plex Sans",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
